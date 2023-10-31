11ca1d00c62174bd378d17ae992b42278205d349
Sokolnikov Bogdan <sokolnikovbogdan1@gmail.com>
2023-10-31 23:01:16 +0300

    added db

--- hello.py
+++ hello.py
@@ -22,6 +22,12 @@ class Users(db.Model):
         return '<Name %r>' % self.name
 
 
+class UserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
 # create a route decaretor
 @app.route('/')
 def index():
@@ -54,3 +60,21 @@ def name():
         flash("Form Submitted Succsesfuly")
 
     return render_template('name.html', name=name, form=form)
+
+@app.route('/user/add', methods=['GET','POST'])
+def add_user():
+    name = None
+    form = UserForm()
+
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+        flash('User Added Successfully')
+    our_users = Users.query.order_by(Users.date_added)
+    return render_template('add_user.html', name=name, form=form, our_users=our_users)
